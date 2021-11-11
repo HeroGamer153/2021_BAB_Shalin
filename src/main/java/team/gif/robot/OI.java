bc957ca628de7480d8d5e21a9b9bc4bd365f9cdc
Shalin Joshi <jshalin21@gmail.com>
2021-11-11 13:34:07 -0600

    Added req 1-4

--- src/main/java/team/gif/robot/OI.java
+++ src/main/java/team/gif/robot/OI.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
 import team.gif.lib.AxisButton;
+import team.gif.robot.commands.RunCIM;
 
 
 public class OI {
@@ -66,6 +67,9 @@ public class OI {
          * dRT.whileHeld(new EjectCommand());
          *
          */
+
+        dB.whileHeld(new RunCIM(true));
+        dX.whileHeld(new RunCIM(false));
 
     }
 
